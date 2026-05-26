d3b73348aaa89c7fb35d27d0b7fff36b8a0c9ce1
Laksh Raj Kumar <lakshrajkumar.791.lrk@gmail.com>
2026-05-26 22:38:37 +0530

    3sum -> optimised Approach

--- Array/2 Pointers Approach/3Sum/OptimisedApproach.cpp
+++ Array/2 Pointers Approach/3Sum/OptimisedApproach.cpp
@@ -14,6 +14,41 @@ public:
     {
        
 
+        sort(nums.begin(),nums.end());
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; //  check  why 
+            }
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else { // match
+
+                    ans.push_back({nums[i] , nums[j] , nums[k] });
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    } // check why 
+                    
+                }
+            }
+        }
+
+        return ans;
 
         
     }
